7995c4f4e9e559d4a3702a903096a349cbf841dd
NemeSnz <neme.d@live.com>
2017-10-31 10:54:14 -0600

    equipment model

---
+++ backend/models/equipment.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const equipmentSchema = new Schema({
+	name: String,
+	category: String,
+	made: String,
+	model: String,
+	per_day_cost: Number,
+	quantity_available: Number,
+	comments: String
+});
+
+const User = mongoose.model('equipment', equipmentSchema);
+
+module.exports = User;
