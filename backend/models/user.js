ae9d89095bbeeddd784dd71b6f9625674c4dc383
NemeSnz <neme.d@live.com>
2017-11-18 11:15:03 -0600

    tool and user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+	email: String,
     name: String,
+    lastName: String,
     password: String
 });
 
